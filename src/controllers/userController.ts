e12c6a073ea13d786c0d075101a45f93d4defa11
diegobiazate <diegorbiazate@gmail.com>
2022-04-25 15:10:57 -0300

    Salvando a imagem no banco

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,7 +1,31 @@
+import {unlink} from 'fs/promises';
 import {Request, Response} from 'express';
+import sharp from 'sharp';
+import { User } from '../models/User';
 
 
 export const upload = async (req: Request, res: Response)=>{
+    let {name, email} = req.body;
 
-    res.json({});
+    if(req.file){
+        const avatar = `${req.file.filename}.jpg`;
+        await sharp(req.file.path)
+            .resize(500,500, {
+                fit: sharp.fit.cover
+            })
+            .toFormat('jpeg')
+            .toFile(`./public/media/${avatar}`);
+        
+        const user = User.build({name, email, avatar});
+        //salvando no banco de dados    
+        await user.save();
+        //deletando arquivo temporário
+        await unlink(req.file.path);
+        
+        //retornando o nome do arquivo
+        res.json({image: `${avatar}`});
+    }else{
+        res.status(400);
+        res.json({error: 'Arquivo inválido'});
+    }
 }
